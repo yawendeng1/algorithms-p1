04d6045faa2c3942629ed8060c9d3071b7ffaf71
yawendeng1 <yd298@cornell.edu>
2017-06-29 16:08:56 -0400

    skeleton of kdtree

---
+++ Kd-Trees/KdTree.java
@@ -0,0 +1,70 @@
+import java.util.TreeSet;
+import java.util.Stack;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+
+public class KdTree {
+    
+    private TreeSet<Point2D> pointSet;
+
+    // construct an empty set of points
+    public KdTree() {
+        pointSet = new TreeSet<>();
+    }
+    
+    // is the set empty?
+    public boolean isEmpty() {
+        return pointSet.isEmpty();
+    }
+    
+    // number of points in the set 
+    public int size() {
+        return pointSet.size();
+    }
+    
+    // add the point to the set (if it is not already in the set)
+    public void insert (Point2D p) {
+        pointSet.add(p);
+    }
+    
+    // does the set contain point p? 
+    public boolean contains (Point2D p) {
+        return pointSet.contains(p);
+    }
+    
+    // draw all points to standard draw 
+    public void draw() {
+        for (Point2D p : pointSet) {
+            p.draw();
+        }
+    }
+    
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range (RectHV rect) {
+        Stack<Point2D> stack = new Stack<>();
+        for (Point2D p : pointSet) {
+            if (rect.contains(p))
+                stack.push(p);
+        }
+        return stack;
+    }
+    
+    // a nearest neighbor in the set to point p; null if the set is empty 
+    public Point2D nearest(Point2D p) {
+        if (pointSet.isEmpty()) return null;
+        Double minDis = Double.MAX_VALUE;
+        Point2D minPoint;
+        for (Point2D point : pointSet) {
+            if (p.distanceTo(point) < minDis) {
+                minDis = p.distanceTo(point);
+                minPoint = point;
+            }
+        }
+        return minPoint;
+    }
+
+    // unit testing of the methods (optional) 
+    public static void main(String[] args) {}
+    
+}
